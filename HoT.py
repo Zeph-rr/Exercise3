f18720b58419e21c3618bb104648ed2f22e6db82
Lance <s1302015@u-aizu.ac.jp>
2024-07-04 22:58:47 +0900

    Code fix

--- HoT.py
+++ HoT.py
@@ -7,12 +7,12 @@ name = input("Who are you?\n")
 print("Hello, " + name + "!")
 
 print("Tossing a coin... ")
-for round in range(1,3):
+for round in range(1,4):
     if random.randint(1,2) == 1:
-        print("Round" + round + ": Heads")
+        print("Round" , round , ": Heads")
         heads += 1
     else:
-        print("Round" + round + ": Tails")
+        print("Round" , round , ": Tails")
         tails += 1
 
-print("Heads: " + heads + "," + "Tails: " + tails)
+print("Heads: " , heads , "," , "Tails: " , tails)
