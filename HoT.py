daf943e37e27c18940be77fea482481b6264fc4d
Lance <s1302015@u-aizu.ac.jp>
2024-07-04 23:06:44 +0900

    Victory message added

--- HoT.py
+++ HoT.py
@@ -3,13 +3,20 @@ import random
 heads = 0
 tails = 0
 
+name = input("Who are you?\n")
+print("Hello, " + name + "!")
+
 print("Tossing a coin... ")
-for round in range(1,3):
+for round in range(1,4):
     if random.randint(1,2) == 1:
-        print("Round" + round + ": Heads")
+        print("Round" , round , ": Heads")
         heads += 1
     else:
-        print("Round" + round + ": Tails")
+        print("Round" , round , ": Tails")
         tails += 1
 
-print("Heads: " + heads + "," + "Tails: " + tails)
+print("Heads: " , heads , "," , "Tails: " , tails)
+if (heads>tails):
+    print("You won!")
+else:
+    print("You lost!")
